dedb074dc9e66b94b0611008abbf8db4ae1d5472
Annanya Sah <92743149+annanyasah7@users.noreply.github.com>
2024-11-22 16:58:51 +0100

    Root container added

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <div>
-      <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <HelloWorld msg="Vite + Vue" />
+  <!-- Root container -->
+  <div id="app">   
+    <AppHeader :search="search" @update-search="updateSearch" />
+    <ContactList :contacts="filteredContacts" />
+    <AddContactForm @add-contact="addContact" />
   </div>
 </template>
 
+<script>
+import { ref } from 'vue'; // Composition API utilities
+import AppHeader from './components/AppHeader.vue';
+import ContactList from './components/ContactList.vue';
+import AddContactForm from './components/AddContactForm.vue';
+
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
+
